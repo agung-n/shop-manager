e0d99d650d999393044e03acd068a4680179b3e9
agung-n <agung.nugraha@live.com>
2021-11-11 17:36:34 +0000

    Initial Commit

---
+++ database/migrations/2021_11_10_072328_create_purchase_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplierId')->constrained('supplier');
+            $table->foreignId('warehouseId')->constrained('warehouse');
+            $table->BigInteger('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase');
+    }
+}
---
+++ database/migrations/2021_11_10_072337_create_invoice_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('clientId')->constrained('client');
+            $table->foreignId('warehouseId')->constrained('warehouse');
+            $table->BigInteger('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
